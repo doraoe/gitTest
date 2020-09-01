634404fec6a72f5407b5bf046153c121ef0ff441
lizhuo <1023191642@qq.com>
2020-09-01 12:23:41 +0800

    更新1

--- demogit/src/main/java/com/example/demogit/Hello.java
+++ demogit/src/main/java/com/example/demogit/Hello.java
@@ -3,5 +3,6 @@ package com.example.demogit;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("更新1");
     }
 }
