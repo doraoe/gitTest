975f5474ec85c0ba0238811180ad3b230ff5b669
lizhuo <1023191642@qq.com>
2020-09-01 12:32:07 +0800

    master添加1

--- demogit/src/main/java/com/example/demogit/Hello.java
+++ demogit/src/main/java/com/example/demogit/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
         System.out.println("更新1");
         System.out.println("更新3");
         System.out.println("创建分支dev");
+        System.out.println("master添加");
     }
 }
